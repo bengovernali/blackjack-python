49f137d6c7bdec339cac28a9274e4f16b2b095e6
Ben Governali <bgovern93@gmail.com>
2023-10-18 14:51:01 -0700

    added player turn logic and score calculation function

--- main.py
+++ main.py
@@ -31,5 +31,30 @@ def show_hands():
     else:
         print(f'CPU Hand: {hands["cpu"]}')
 
+def calculate_score(user):
+    score = sum(hands[user])
+    #if score > 21 and 11 in hand, replace with 1 then recalculate score.
+    if score > 21 and 11 in hands[user]:
+        index_of_ace = hands[user].index(11)
+        hands[user][index_of_ace] = 1
+        score = sum(hands[user])
+    print(score)
+    return score
+
+def player_turn():
+    # prompt the user to draw a card
+    user_choice = input("Would you like to draw a card? Please type 'y' for yes or 'n' for no. ")
+    if user_choice == 'n':
+        cpu_turn()
+    else:
+        hands["player"].append(random.choice(cards))
+        calculate_score("player")
+
+def cpu_turn():
+    print("CPU TURN")
+
 deal_cards()
 show_hands()
+calculate_score("player")
+calculate_score("cpu")
+player_turn()
